fe881c183c370174f5013b073cdfcd553740b523
navyasrikarri <141988192+navyasrikarri@users.noreply.github.com>
2024-05-22 15:26:37 +0530

    Double linkedlist construction - DOUBLE LL.py

--- Double linkedlist construction - DOUBLE LL.py
+++ Double linkedlist construction - DOUBLE LL.py
@@ -21,4 +21,29 @@ def insertAtEnd(head, ele):
        return newNode
    tail = findTail(head)
    tail.next = newNode
-   newNode.prev = tail
+   newNode.prev = tail 
+return head
+
+
+def printLeftToRight(head):
+   curr = head
+   while curr != None:
+       print(curr.data, end = " ")
+       curr = curr.next
+   print()
+  
+def printRightToLeft(head):
+   tail = findTail(head)
+   while tail != None:
+       print(tail.data, end = " ")
+       tail = tail.prev
+   print()
+      
+n = int(input())
+l = list(map(int, input().split()))
+head = None
+for ele in l:
+   head = insertAtEnd(head, ele)
+printLeftToRight(head)
+printRightToLeft(head)
+
